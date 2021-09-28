89a6ab618b69c11db7786a776677dfda56f2adde
Chris Mahoney <chris.p.mahoney@gmail.com>
2021-09-28 13:21:19 -0500

    Added axios, login form components, beginning of Auth service component

--- Exercise1/client/src/Pages/Login.js
+++ Exercise1/client/src/Pages/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { 
+  Controller,
+  useForm 
+} from 'react-hook-form';
 
 import {
-  AppBar,
-  Toolbar,
   Typography,
-  Container
+  Container,
+  TextField,
+  Grid
 } from '@mui/material';
 
 const Login = () => {
+  const { register, handleSubmit, control, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
     <>
@@ -15,6 +21,37 @@ const Login = () => {
         <Typography variant="h4" style={{ textAlign: 'center' }}>
           Login
         </Typography>
+      </Container>
+      <Container style={{ margin: 20 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={4}>
+            <Grid item xs="12">
+              <Controller
+                name="username"
+                control={control}
+                defaultValue={""}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField label="username" {...field} />
+                )}
+              />
+            </Grid>
+            <Grid item xs="12">
+              <Controller
+                name="password"
+                control={control}
+                defaultValue={""}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField label="password" type="password" {...field} />
+                )}
+              />
+            </Grid>
+            <Grid item xs="12">
+              <input type="submit" />
+            </Grid>
+          </Grid>
+        </form>
       </Container>
     </>
   );
